54f165d4c19523291faa61914dd04b4015202273
Dag Frode Solberg <dag.solberg@bekk.no>
2021-04-08 15:17:35 +0200

    Improved flow

--- src/qr-scanner/index.tsx
+++ src/qr-scanner/index.tsx
@@ -1,3 +1,4 @@
+
 import jsQR from "jsqr";
 import React, { useState } from "react";
 import { ResizeImageInput } from "../resize-image-input";
@@ -7,6 +8,7 @@ import "./index.css";
 Based on https://github.com/cozmo/jsQR/blob/master/docs/index.html 
 and https://betterprogramming.pub/add-an-html-canvas-into-your-react-app-176dab099a79
 Zoom from: https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Image_Capture_API
+
 */
 
 type ScannerProps = {
@@ -19,10 +21,11 @@ const Scanner = ({ onChange }: ScannerProps): JSX.Element => {
 
   const videoRef = React.useRef<HTMLVideoElement>(null);
   const [video, setVideo] = useState<HTMLVideoElement | null>(null);
-  const [code, setCode] = useState("")
+  const [code, setCode] = useState("");
   const [showScanImageButton, setShowScanImageButton] = useState(true);
+  const [error, setError] = useState("");
+  const [img, setImg] = useState("");
   let scanVideoStreamForQRCodeTimeoutId: NodeJS.Timeout;
-
 
   const scanImageForQRCode = (
     imageSource: CanvasImageSource,
@@ -42,28 +45,26 @@ const Scanner = ({ onChange }: ScannerProps): JSX.Element => {
         if (code) {
           setCode(code.data);
 
-          setTimeout(() => {
-            onChange(code.data);
-          }, 1000);
           return true;
         }
       } catch (error) {
-        if (error! instanceof RangeError) throw error;
+        if (!(error instanceof RangeError)) throw error;
       }
     }
     return false;
   };
-  
 
   const scanVideoStreamForQRCode = (): void => {
-
-    if(code ){
+    if (code) {
       return;
     }
     if (video && video.readyState === video.HAVE_ENOUGH_DATA) {
       scanImageForQRCode(video, video.videoHeight, video.videoWidth);
-    }  
-    scanVideoStreamForQRCodeTimeoutId =  setTimeout(scanVideoStreamForQRCode, 200)
+    }
+    scanVideoStreamForQRCodeTimeoutId = setTimeout(
+      scanVideoStreamForQRCode,
+      200
+    );
   };
 
   React.useEffect(() => {
@@ -78,14 +79,15 @@ const Scanner = ({ onChange }: ScannerProps): JSX.Element => {
           mediaStream = stream;
 
           const [track] = mediaStream.getVideoTracks();
-          
-          const capabilities = track && track.getCapabilities && track.getCapabilities();
+
+          const capabilities =
+            track && track.getCapabilities && track.getCapabilities();
 
           if (capabilities && capabilities.zoom) {
-         
-            track && track.applyConstraints({
-              advanced: [{ zoom: capabilities.zoom.min }],
-            });
+            track &&
+              track.applyConstraints({
+                advanced: [{ zoom: capabilities.zoom.min }],
+              });
           }
 
           video.srcObject = mediaStream;
@@ -93,42 +95,81 @@ const Scanner = ({ onChange }: ScannerProps): JSX.Element => {
           // required to tell iOS safari we don't want fullscreen
           video.setAttribute("playsinline", "true");
           video.play();
-          
+
           scanVideoStreamForQRCodeTimeoutId = setTimeout(
             scanVideoStreamForQRCode,
             1000
           );
-        }).then(() => {
-          setShowScanImageButton(false)
         })
+        .then(() => {
+          setShowScanImageButton(false);
+        });
     }
     const cleanup = (): void => {
       // release webcam when it is no longer needed
       mediaStream?.getTracks().forEach((t) => {
         t.stop();
       });
-      clearTimeout( scanVideoStreamForQRCodeTimeoutId);
+      clearTimeout(scanVideoStreamForQRCodeTimeoutId);
     };
     return cleanup;
   }, [video]);
 
-  const getQRFromBase64String = (base64EncodedImage: string) : void =>{
-    const img = new Image()
+  const getQRFromBase64String = (base64EncodedImage: string): void => {
+    setError("");
+    setCode("");
+    setImg(base64EncodedImage);
+
+    const img = new Image();
     img.onload = () => {
       const foundQrCode = scanImageForQRCode(img, img.width, img.height);
       if (!foundQrCode) {
-        alert("could not find QR code im uploaded image. Please try again")
+        setError(
+          "Fant ikke QR kode i bilde. Prøv å holde mobilen litt lenger unna og ta et nytt bilde"
+        );
       }
-    }
+    };
     img.src = base64EncodedImage;
-  } 
+  };
 
   return (
     <div className="scanner">
-      {showScanImageButton && <ResizeImageInput label="Last opp bilde av QR code" onChange={getQRFromBase64String} maxWidth={640} maxHeight={480} />}
-      <video className="scanner__video" ref={videoRef}></video>
+      <video
+        className={`scanner__video ${
+          showScanImageButton ? "scanner__video--hidden" : ""
+        }`}
+        ref={videoRef}
+      ></video>
+      {img && <img className="scanner__image" alt="Uploaded image" src={img} />}
       <div className="scanner__code">{code}</div>
-      <button onClick={() => onChange("")}>Avbryt</button>
+
+      <div className="scanner__error">{error}</div>
+      <div id="scanner__buttons">
+        {showScanImageButton && (
+          <ResizeImageInput
+            label="Last opp bilde av QR code"
+            onChange={getQRFromBase64String}
+            maxWidth={640}
+            maxHeight={480}
+            showImage={false}
+            buttonClassName="scanner__buttons__background"
+          />
+        )}
+        {code && (
+          <button
+            className="scanner__buttons__background"
+            onClick={() => onChange(code)}
+          >
+            Lagre
+          </button>
+        )}
+        <button
+          className="scanner__buttons__background"
+          onClick={() => onChange("")}
+        >
+          Avbryt
+        </button>
+      </div>
     </div>
   );
 };
@@ -140,9 +181,16 @@ type QRScannerProps = {
 export const QRScanner = ({ onChange, label }: QRScannerProps): JSX.Element => {
   const [showQRScanner, setShowQRScanner] = useState(false);
 
+
   return (
     <div className="qr-scanner">
-      <button onClick={() => setShowQRScanner(true)}>{label}</button>
+      <button
+        onClick={() => {
+          setShowQRScanner(true);
+        }}
+      >
+        {label}
+      </button>
       {showQRScanner ? (
         <Scanner
           onChange={(code) => {
--- src/resize-image-input/index.tsx
+++ src/resize-image-input/index.tsx
@@ -1,88 +1,98 @@
 import React, { useState } from "react";
-import "./index.css";
-
 
 type ImageInputProps = {
-  label: string
-  onChange: (value: string) => void
-  maxWidth?: number
-  maxHeight?: number
-}
+  label: string;
+  onChange: (value: string) => void;
+  maxWidth?: number;
+  maxHeight?: number;
+  showImage?: boolean;
+  buttonClassName?: string;
+};
 
 export const ResizeImageInput = ({
   label,
   maxWidth = 1920,
   maxHeight = 1080,
-  onChange
+  onChange,
+  showImage = true,
+  buttonClassName = "",
 }: ImageInputProps): JSX.Element => {
-  const [image, setImage] = useState("")
+  const [image, setImage] = useState("");
 
   const uploadImage = (): void => {
-    const input = document.createElement("input")
-    input.type = "file"
-    input.accept = "image/*"
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
 
     input.onchange = (event: Event) => {
       /* Use file reader to read image from client*/
-      const fr = new FileReader()
+      const fr = new FileReader();
 
       /* When the image is loaded, transform and resize it*/
       fr.onload = () => {
         /* Get the result of the loaded image */
-        const readImageByteString = fr.result?.toString() || ""
+        const readImageByteString = fr.result?.toString() || "";
 
         /* Create a canvas to resize the image to preferred size */
-        const imageResizeCanvas = document.createElement("canvas")
+        const imageResizeCanvas = document.createElement("canvas");
 
-        const imageResizeCanvasContext = imageResizeCanvas.getContext("2d")
+        const imageResizeCanvasContext = imageResizeCanvas.getContext("2d");
 
         if (imageResizeCanvasContext) {
           /* Create an image object that can be drawn on a canvas */
-          const imageObjectToBeDrawnOnCanvas = new Image()
+          const imageObjectToBeDrawnOnCanvas = new Image();
 
           /* Wait til image has been loaded before trying to draw it on canvas */
           imageObjectToBeDrawnOnCanvas.onload = () => {
             /* Calculate image height and width */
             let imageWidth = maxWidth;
             let imageHeight = maxHeight;
-            const xScale = maxWidth / imageObjectToBeDrawnOnCanvas.width
-            const yScale = maxHeight / imageObjectToBeDrawnOnCanvas.height
+            const xScale = maxWidth / imageObjectToBeDrawnOnCanvas.width;
+            const yScale = maxHeight / imageObjectToBeDrawnOnCanvas.height;
             if (xScale > yScale) {
-              imageHeight = imageObjectToBeDrawnOnCanvas.height * xScale
+              imageHeight = imageObjectToBeDrawnOnCanvas.height * xScale;
             } else {
-              imageWidth = imageObjectToBeDrawnOnCanvas.width * yScale
+              imageWidth = imageObjectToBeDrawnOnCanvas.width * yScale;
             }
 
             /* Draw the image scaled down */
-            imageResizeCanvas.width = imageWidth
-            imageResizeCanvas.height = imageHeight
-            imageResizeCanvasContext.drawImage(imageObjectToBeDrawnOnCanvas, 0, 0, imageWidth, imageHeight)
+            imageResizeCanvas.width = imageWidth;
+            imageResizeCanvas.height = imageHeight;
+            imageResizeCanvasContext.drawImage(
+              imageObjectToBeDrawnOnCanvas,
+              0,
+              0,
+              imageWidth,
+              imageHeight
+            );
 
-            const base64ImageString = imageResizeCanvas.toDataURL()
-            setImage(base64ImageString)
-            onChange(base64ImageString)
-          }
+            const base64ImageString = imageResizeCanvas.toDataURL();
+            setImage(base64ImageString);
+            onChange(base64ImageString);
+          };
 
-          imageObjectToBeDrawnOnCanvas.src = readImageByteString
+          imageObjectToBeDrawnOnCanvas.src = readImageByteString;
         }
-      }
+      };
       if (event && event.target) {
-        const target = event.target as HTMLInputElement
+        const target = event.target as HTMLInputElement;
         if (target.files) {
-          fr.readAsDataURL(target.files[0])
+          fr.readAsDataURL(target.files[0]);
         }
       }
 
       /* TODO split into multiple functions for better readability */
-    }
+    };
 
-    input.click()
-  }
+    input.click();
+  };
 
-  return     (<div className="input-image">
-      {image && <img alt="Uploaded image" className="input-image--image" src={image} />}
-
-      <button onClick={uploadImage}>{label}</button>
-    </div>)
-  
-}
+  return (
+    <div className="input-image">
+      {showImage && <img className="input-image--image" src={image} />}
+        <button className={buttonClassName} onClick={uploadImage}>
+          {label}
+        </button>
+    </div>
+  );
+};
